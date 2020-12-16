a4e0f305ce937126ba56438dde0054062d5a2eee
allen-yyy <59130189+allen-yyy@users.noreply.github.com>
2020-12-16 19:26:08 +0800

    1

--- haribotefs_libc/com.haribote.fs.c
+++ haribotefs_libc/com.haribote.fs.c
@@ -4,9 +4,9 @@
 */ 
 #include "com.haribote.fs.inc.h"
 
-void s1_alloc(struct s1 a_s1)
+void s1_alloc(struct s1 *a_s1)
 {
-	a_s1.E_flag = E_FLAG;
-	a_s1.E_n = 1;
+	a_s1->E_flag = E_FLAG;
+	a_s1->E_n = 1;
 }
 
--- haribotefs_libc/com.haribote.fs.inc.h
+++ haribotefs_libc/com.haribote.fs.inc.h
@@ -5,40 +5,41 @@
 		Allen		2020.10.19		date s1 sn go bsmar fren root
 		Allen		2020.11.6		R_FLAG DA_FLAG FD_FLAG
 		Allen		2020.11.22		R_FLAG DA_FLAG FD_FLAG S_FLAG E_FLAG
+		Allen		2020.12.16		log LOG_COPY LOG_WRITE LOG_READ
 */
 #include "bootpack.h"
 #define FS_NAME "hafs"
 
-struct fren{
+struct fren {
 	int addrH;
 	int addrL;
 	long size;
 };
 
-struct map{
+struct map {
 	int frens;
-	struct fren[]; 
+	struct fren[];
 };
 
-struct go{
+struct go {
 	char *filesystem;
-	int s_long;	
+	int s_long;
 };
 
-struct s1{
+struct s1 {
 	int E_flag;
 	int E_n;
 	int E_type;
 	struct go go_s1;
 };
 
-struct sn{
+struct sn {
 	int s_flag;
 	int s_n;
 	struct go gos;
 };
 
-struct bsmar{
+struct bsmar {
 	int m_flag;
 	int st_flag;
 	int hd_j_s1;
@@ -46,13 +47,11 @@ struct bsmar{
 	struct map map1;
 };
 
-struct date
-{
+struct date {
 	int y1,y2;
 	int m,day;
 };
-struct FDINFO
-{
+struct FDINFO {
 	int fd_flag;
 	int filenamel;
 	char *filename;
@@ -61,8 +60,8 @@ struct FDINFO
 	int s1,s2,off;
 	int fs1,fs2,foff;
 	int type;
-	struct date cdate; //创建 
-	struct date gdate; //访问 
+	struct date cdate; //创建
+	struct date gdate; //访问
 	struct date wdate; //修改
 	int fsize1,fsize2,fsize3;
 	char root1[3];
@@ -70,8 +69,7 @@ struct FDINFO
 	char Adn;
 };
 
-struct FILED
-{
+struct FILED {
 	int da_flag;
 	int fl1,fl2,fl3;
 	int n_s[3];
@@ -82,16 +80,23 @@ struct FILED
 	int n_s1,n_s2,n_off;
 };
 
-struct root{
+struct root {
 	int r_flag;
 	struct FDINFO files[];
 	int s_n;
 };
 
-#define R_FLAG 0x52525252 
-#define DA_FLAG 0x44414441 
-#define FD_FLAG 0x46444644 
+#define R_FLAG 0x52525252
+#define DA_FLAG 0x44414441
+#define FD_FLAG 0x46444644
 #define S_FLAG 0x53535353
 #define E_FLAG 0x45454545
-	
-																							                                            
+
+struct log {
+	char log[0x30];
+	int type;
+};
+
+#define LOG_COPY 0x1
+#define LOG_WRITE 0x2
+#define LOG_READ 0x3 
