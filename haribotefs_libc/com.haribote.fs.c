637e2177b97a3c56d6f1b93495c391b1f2179e63
allen-yyy <59130189+allen-yyy@users.noreply.github.com>
2021-01-20 19:00:18 +0800

    fs
    fs

--- haribotefs_libc/com.haribote.fs.c
+++ haribotefs_libc/com.haribote.fs.c
@@ -1,6 +1,7 @@
 /* This Haribote's FS c file
 		name       Date
 		Allen      2020.10.6
+		Allen      2021.1.20	disk_check
 */ 
 #include "com.haribote.fs.inc.h"
 
@@ -10,3 +11,7 @@ void s1_alloc(struct s1 *a_s1)
 	a_s1->E_n = 1;
 }
 
+int disk_check(int *disk)
+{
+	
+}
