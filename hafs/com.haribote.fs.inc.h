a9448cec35b42e77f820260e7f29adc0ec071f3e
allen-yyy <59130189+allen-yyy@users.noreply.github.com>
2021-10-15 19:05:24 +0800

    Update com.haribote.fs.inc.h

--- hafs/com.haribote.fs.inc.h
+++ hafs/com.haribote.fs.inc.h
@@ -11,6 +11,7 @@
 #define FS_VERSION _VERSION(1,0) 
 #define SEG_LONG 8192
 #define BLOCK_LONG 4096
+#define NAMELENMAX 256
 
 struct seg1{
 	int flag;
@@ -20,6 +21,7 @@ struct seg1{
 
 struct segn{
 	int flag;
+	int segn;
 	char filesys[6];
 };
 
@@ -38,36 +40,49 @@ struct bsmar{	//like ext2 fs's super block.
 	struct map maps;
 	char filesys[6];
 	int version;
+	int device;
+	#define DEVTYPE_HD 1
+	#define DEVTYPE_FD 2
+	#define DEVTYPE_USB 3
 	int allsize1;
 	int allsize2;
 	int allsize3;
 	int allsize4;
 	int os;		//usually is 1--hariboteos
-	int seg_a_bit;	//usually is 128 
+	int seg_a_bit;	//usually is 128
+	int status; //now fs status
+	#define STATUS_CLEAN 1<<0
+	#define STATUS_WRITE 1<<1
+	#define STATUS_ERROR 1<<2
+	//1742B free
+	char pad1[2];
+	
+	int pad[435];
 };
 
 struct FILED{
 	int flag;
-	char filename[30];
-	char fileext[10];
-	int fsegL;
-	int fsegM;
-	int fsegH;
-	int flongL;
-	int flongH;
-	long flongl;
-	char dir;
+	int fsegL;//use it*1
+	int fsegM;//use it*512
+	int fsegH;//use it*512,too
+	int flongL;//use B
+	int flongM;//use KB
+	int flongH;//use KB,too
+	char isdir;
 	char root[3];
+	int  sizeofthis;
+	int filenamelen; 
+	char filename[30];
 };
 
 struct FILEA{
 	int flag;
-	int longL;
-	int longH;
-	long longl;
+	int longL;//use B
+	int longH;//use KB
+	long longl;//use KB,too
 };
 
-#define segflag		0x6e676573	//seg1
+#define segflag		0x6e676573	//segn
 #define bsmarflag	0x616d7362  //bsma
 #define etcflag		0x31637465	//etc1
 #define fileflag	0x656c6966	//file
@@ -79,6 +94,16 @@ struct rootdir{
 };
 
 struct log{
+	int many;
+	struct log_a logs[0];
+};
+
+struct log_a{
 	int type;
-	int shutflag;
+	#define LOG_READ 1<<0
+	#define LOG_WRITE 1<<1
+	#define LOG_EXDEV 1<<1	
+	int segA;//it*1
+	int segB;//it*10
+	int segC;//it*10
 };
